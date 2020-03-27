cd7728ceb2e678a90bca96b712ff86d85a52bf41
MarlyasDad <yudaev.alex@gmail.com>
2020-03-27 14:20:14 +0300

    Update example.py

--- example.py
+++ example.py
@@ -1,7 +1,7 @@
 from loto.core import LotoGame
 
 if __name__ == '__main__':
-    computers_count = 1
+    computers_count = 2
     humans_count = 1
 
     game = LotoGame(computers_count, humans_count)
