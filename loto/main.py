8ad13f8898dbb682223a93f0e8b4c8d04d283f6f
MarlyasDad <yudaev.alex@gmail.com>
2020-03-30 14:22:33 +0300

    Update example.py and main.py

--- loto/main.py
+++ loto/main.py
@@ -1,10 +1,15 @@
 import sys
 from loto.core import LotoGame, Config
 
-if __name__ == '__main__':
+
+def main():
     config = Config(sys.argv)
     # Or enter the parameters manually
     # config.computers = 2
     # config.humans = 2
     game = LotoGame(config)
     game.run()
+
+
+if __name__ == '__main__':
+    main()
